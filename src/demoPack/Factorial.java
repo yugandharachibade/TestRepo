77fde4af7b5b4e691e27c11ff171101bd2ab7416
unknown <ychibade@gmail.com>
2019-02-27 10:24:07 +0530

    first time added

---
+++ src/demoPack/Factorial.java
@@ -0,0 +1,28 @@
+package demoPack;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+				// TODO Auto-generated method stub
+			 int a;
+			 int temp=1;
+			 Scanner s=new Scanner(System.in);
+			 System.out.println("Enter any number");
+			 a=s.nextInt();
+			  for(int i=1; i<=a;i++)
+			  {
+				temp=temp*i;
+				 
+			  }
+			  System.out.println("Factorial:" +temp);
+
+			}
+
+		
+}
